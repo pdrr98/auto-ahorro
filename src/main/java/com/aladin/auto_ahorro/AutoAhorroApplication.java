d97bb546fa329241e8860b39d1a266cf45632d59
pdrr98 <pedro.98.gmr@gmail.com>
2025-07-18 23:09:56 -0600

    Spring proyect initialization

---
+++ src/main/java/com/aladin/auto_ahorro/AutoAhorroApplication.java
@@ -0,0 +1,13 @@
+package com.aladin.auto_ahorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAhorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoAhorroApplication.class, args);
+	}
+
+}
